a597df3105b21a0812e0cd9235e1a25b62a4f645
Taalay Joldoshev <gmoftj@gmail.com>
2024-01-13 21:00:24 +0600

    Done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        var profile = new Profile();
+        try (var br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                var keyValue = line.split(":", 2);
+                var key = keyValue[0].trim();
+                var val = keyValue[1].trim();
+
+                switch (key) {
+                    case "Name":
+                        profile.setName(val);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(val));
+                        break;
+                    case "Email":
+                        profile.setEmail(val);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(val));
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
